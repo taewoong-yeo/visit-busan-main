ed6c39a2ca6cd3979f54ad92974a898d055463e7
taewoong <92969045+taewoong-yeo@users.noreply.github.com>
2023-06-26 10:38:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("nav,.dropbox,.icon").hover(function(){
         $(".sub,.dropbox,.logo2").stop().slideToggle();
     });
-    
+    alert(1920 사이즈 최적화입니다.);
     
 });    
 
